13244d5193f5410dc52ca351da99a3f8deb29b67
Shunsuke Sakai <s08050140311ss@gmail.com>
2026-06-18 02:37:30 +0000

    Implement Supabase Auth

--- lib/auth.ts
+++ lib/auth.ts
@@ -22,8 +22,5 @@ export async function getCurrentUser() {
       email: user.email!,
       displayName,
     },
-  }).catch((e) => {
-    console.error("[getCurrentUser] prisma upsert error:", e);
-    throw e;
   });
 }
